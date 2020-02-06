1a2349f2d9501f4b199e9fa7bfe494f9b531a3e9
Brian <rskace@gmail.com>
2020-02-06 11:27:13 -0800

    actions and images

--- SwiftCl2SlotGame/SwiftCl2SlotGame/ViewController.swift
+++ SwiftCl2SlotGame/SwiftCl2SlotGame/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView1: UIImageView!
+    
+    @IBOutlet weak var imgView2: UIImageView!
+    
+    @IBOutlet weak var imgView3: UIImageView!
+    
+    let imageNames = ["cherries", "dollarsign", "lemon", "grapes", "number7"]
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
